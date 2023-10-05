45f218581b09070a94134504667b7b497f96acc9
MathLuz <111208300+MathLuz@users.noreply.github.com>
2023-10-04 23:12:59 -0300

    Atualizando

---
+++ build/app/generated/source/buildConfig/release/com/example/helphealth/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.helphealth;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.helphealth";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
